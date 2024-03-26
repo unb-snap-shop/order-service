3324e8e557a2b225c0aeb5ad4b185801b0cbf8b3
Eric599 <cuenat.eric@gmail.com>
2024-03-26 19:06:53 -0300

    add code and endpoint for view cart

--- src/order_service_api/routes.py
+++ src/order_service_api/routes.py
@@ -1,19 +1,26 @@
 from flask import Flask, Blueprint, request, jsonify, make_response
-from ..utils.manage_cart import write_json
+from ..utils.manage_cart import write_json, get_json
 from ..observers.ui_observer import UIObserver
+from flask_cors import CORS
 
 order_service_api = Blueprint('order_service_api', __name__)
+
+CORS(order_service_api)
 
 ui_observer = UIObserver()
 
 @order_service_api.route('/add_to_cart', methods=['POST'])
 def add_to_cart():
     data = request.json  # json from UI
-    product_id = data.get('product_id')
-    quantity = data.get('quantity')
+    product_id = data.get('productId')
+    product_brand = data.get('productBrand')
+    product_name = data.get('productName')
+    product_price = data.get('productPrice')
+
+    
 
     # Perform validation
-    if not product_id or not quantity:
+    if not product_id or not product_brand or not product_name or not product_price:
         return jsonify({'error': 'Invalid request'}), 400
 
     # Add product to cart (pseudocode)
@@ -21,7 +28,18 @@ def add_to_cart():
     # Update database with new cart information
     write_json(data)
     ui_observer.update('Product added to cart successfully')
-    return jsonify({'message': 'Product added to cart successfully'}), 200
+    response = make_response(jsonify({'message': 'Product added to cart successfully'}), 200)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    
+    return response
+
+
+@order_service_api.route('/get_cart', methods=['GET'])
+def get_cart_contents():
+    cart = get_json()
+    response = make_response(jsonify(cart))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @order_service_api.route('/test', methods=['GET'])
 def test_endpoint():
--- src/utils/manage_cart.py
+++ src/utils/manage_cart.py
@@ -4,7 +4,7 @@ filename = 'src/data/cart.json'
 
 def write_json(new_data):
     with open(filename,'r+') as file:
-          # First we load existing data into a dict.
+        # First we load existing data into a dict.
         file_data = json.load(file)
         # Join new_data with file_data inside
         file_data.append(new_data)
@@ -14,3 +14,8 @@ def write_json(new_data):
         json.dump(file_data, file, indent = 4)
         
         file.truncate()
+
+def get_json():
+    with open(filename,'r+') as file:
+      file_data = json.load(file)
+      return file_data
